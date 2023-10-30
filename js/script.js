206689edd96b767a2682bf34b9c1c50129650fa6
vitaliishevchuk <vitaliishevchuk123@gmail.com>
2023-10-30 11:54:03 +0200

    overflow-hidden

--- js/script.js
+++ js/script.js
@@ -1,17 +1,20 @@
 window.addEventListener('DOMContentLoaded', () => {
     const menu = document.querySelector('.nav-menu'),
     menuItems = document.querySelectorAll('.nav-menu__list-item'),
-    hamburger = document.querySelector('.hamburger');
+    hamburger = document.querySelector('.hamburger'),
+    bodyElement = document.querySelector('body');
 
     hamburger.addEventListener('click', () => {
         hamburger.classList.toggle('hamburger_active');
         menu.classList.toggle('nav-menu_active');
+        bodyElement.classList.toggle('overflow-hidden');
     });
 
     menuItems.forEach(menuItem => {
         menuItem.addEventListener('click', () => {
             hamburger.classList.toggle('hamburger_active');
             menu.classList.toggle('nav-menu_active');
+            bodyElement.classList.toggle('overflow-hidden');
         })
     })
 })
