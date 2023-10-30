ebd6d85e41b190106cb5f39b352dcb4de45102e3
vitaliishevchuk <vitaliishevchuk123@gmail.com>
2023-10-30 12:04:12 +0200

    fix close menu

--- js/script.js
+++ js/script.js
@@ -1,20 +1,29 @@
 window.addEventListener('DOMContentLoaded', () => {
     const menu = document.querySelector('.nav-menu'),
-    menuItems = document.querySelectorAll('.nav-menu__list-item'),
-    hamburger = document.querySelector('.hamburger'),
-    bodyElement = document.querySelector('body');
+        menuItems = document.querySelectorAll('.nav-menu__list-item'),
+        hamburger = document.querySelector('.hamburger'),
+        bodyElement = document.querySelector('body');
 
-    hamburger.addEventListener('click', () => {
+    function toggleMenu() {
         hamburger.classList.toggle('hamburger_active');
         menu.classList.toggle('nav-menu_active');
         bodyElement.classList.toggle('overflow-hidden');
+    }
+
+    hamburger.addEventListener('click', () => {
+        toggleMenu();
     });
 
     menuItems.forEach(menuItem => {
         menuItem.addEventListener('click', () => {
-            hamburger.classList.toggle('hamburger_active');
-            menu.classList.toggle('nav-menu_active');
-            bodyElement.classList.toggle('overflow-hidden');
+            toggleMenu();
         })
     })
+
+    // Додамо обробник кліку для закриття меню при кліку поза ним
+    document.addEventListener('click', (event) => {
+        if (menu.classList.contains('nav-menu_active') && event.target !== hamburger && !menu.contains(event.target)) {
+            toggleMenu();
+        }
+    });
 })
